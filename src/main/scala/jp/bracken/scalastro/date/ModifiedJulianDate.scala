9f558122ea4e7a384193b3f390f3d656ac95e9e2
Chris Bracken <chris@bracken.jp>
2013-03-18 23:11:51 -0700

    Minor doc updates

--- src/main/scala/jp/bracken/scalastro/date/ModifiedJulianDate.scala
+++ src/main/scala/jp/bracken/scalastro/date/ModifiedJulianDate.scala
@@ -15,14 +15,16 @@ import org.joda.time.ReadableInstant
   * since the epoch: 17 November 1858 CE 0:00:00.000 (midnight). More
   * historically accurately, Modified Julian Date = Julian Date - 2 400 000.5.
   *
-  * Modified Julian Date has two main benefits over Julian Date: first, the
+  * Modified Julian Date differs from Julian Date in two regards: first, the
   * start of the day aligns with civil time reckoning, at midnight. Second,
   * since the high-order digits of Julian Dates rarely change, it results in
   * shorter 5-digit dates (for the time being).
   *
   * More information about Modified Julian Date available at the
   * [[http://tycho.usno.navy.mil/mjd.html US Naval Observatory Time Service
-  * Department]].
+  * Department]] and at the
+  * [[http://www.iers.org/nn_10910/IERS/EN/Science/Recommendations/resolutionB1.html International
+  * Earth Rotation and Reference Systems Service]].
   */
 object ModifiedJulianDate {
   private val epoch = new DateTime(1858, 11, 17, 0, 0, 0, 0, DateTimeZone.UTC)
