ba3ab9b1ecd5e764b3e56c2b642a4d329ee92408
Chris Bracken <chris@bracken.jp>
2013-03-18 23:06:22 -0700

    Added docs.

--- src/main/scala/jp/bracken/scalastro/date/JulianDate.scala
+++ src/main/scala/jp/bracken/scalastro/date/JulianDate.scala
@@ -8,14 +8,28 @@ import org.joda.time.Days
 import org.joda.time.Duration
 import org.joda.time.ReadableInstant
 
+/** Transformations between Joda instants and double representations of Julian
+  * Dates (JD).
+  *
+  * Julian Date is the count of days (including fractions of a day) since the
+  * epoch: 1 January 4713 BCE at 12:00:00.000 (noon) of the Julian Proleptic
+  * Calendar. It is in wide use by astronomers, geophysicists and others who
+  * require an unambiguous dating system based on a continuous count of days.
+  *
+  * More information from the
+  * [[http://www.iers.org/nn_10910/IERS/EN/Science/Recommendations/resolutionB1.html International
+  * Earth Rotation and Reference Systems Service]].
+  */
 object JulianDate {
   private val epoch = new DateTime(-4713, 1, 1, 12, 0, 0, 0,
       JulianChronology.getInstance(DateTimeZone.UTC))
   private val oneDay = Days.ONE.toStandardDuration.getMillis.doubleValue
 
+  /** Returns the Julian Date for the specified instant. */
   def doubleValue(instant:ReadableInstant): Double =
     new Duration(epoch, instant).getMillis / oneDay
 
+  /** Returns a DateTime in ISOChronology for the specified Julian Date. */
   def fromDouble(julianDate:Double): DateTime =
     epoch.plus(Duration.millis((julianDate * oneDay).round)).toDateTime(
       ISOChronology.getInstance(DateTimeZone.UTC))
--- src/main/scala/jp/bracken/scalastro/date/ModifiedJulianDate.scala
+++ src/main/scala/jp/bracken/scalastro/date/ModifiedJulianDate.scala
@@ -8,13 +8,33 @@ import org.joda.time.Days
 import org.joda.time.Duration
 import org.joda.time.ReadableInstant
 
+/** Transformations between Joda instants and double representations of Modified
+  * Julian Date (MJD).
+  *
+  * Modified Julian Date is the count of days (including fractions of a day)
+  * since the epoch: 17 November 1858 CE 0:00:00.000 (midnight). More
+  * historically accurately, Modified Julian Date = Julian Date - 2 400 000.5.
+  *
+  * Modified Julian Date has two main benefits over Julian Date: first, the
+  * start of the day aligns with civil time reckoning, at midnight. Second,
+  * since the high-order digits of Julian Dates rarely change, it results in
+  * shorter 5-digit dates (for the time being).
+  *
+  * More information about Modified Julian Date available at the
+  * [[http://tycho.usno.navy.mil/mjd.html US Naval Observatory Time Service
+  * Department]].
+  */
 object ModifiedJulianDate {
   private val epoch = new DateTime(1858, 11, 17, 0, 0, 0, 0, DateTimeZone.UTC)
   private val oneDay = Days.ONE.toStandardDuration.getMillis.doubleValue
 
+  /** Returns the Modified Julian Date for the specified instant. */
   def doubleValue(instant:ReadableInstant): Double =
     new Duration(epoch, instant).getMillis / oneDay
 
+  /** Returns a DateTime in ISOChronology for the specified Modified Julian
+    * Date.
+    */
   def fromDouble(modifiedJulianDate:Double): DateTime =
     epoch.plus(Duration.millis((modifiedJulianDate * oneDay).round))
 }
